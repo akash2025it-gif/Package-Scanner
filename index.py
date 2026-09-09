f7f3bcfd3869c3c5abcb7836f3db15027747d769
akash2025it-git <akash2025it@gmail.com>
2026-09-09 12:00:47 +0530

    Make Vercel dependency build compatible

--- index.py
+++ index.py
@@ -4,7 +4,7 @@ from pathlib import Path
 import sys
 
 
-BACKEND_DIR = Path(__file__).resolve().parents[1] / "backend"
+BACKEND_DIR = Path(__file__).resolve().parent / "backend"
 backend_dir_string = str(BACKEND_DIR)
 if backend_dir_string not in sys.path:
     sys.path.insert(0, backend_dir_string)
